a74d7243a39bf978a56c790b40c935556b6fa99b
Alankazamm <bruno.alanc61@gmail.com>
2022-05-08 19:00:47 -0300

    legal issues kk

--- atw.js
+++ atw.js
@@ -5,16 +5,6 @@ const mediaQuery = window.matchMedia("(orientation: portrait)");
 
 
 
-if (mediaQuery.matches) {
-    document.getElementById('container').style.cssText = 
-    "transform: rotate(-90deg);transform-origin: left top;width: 100vh;height: 100vw;overflow-x: hidden;position:absolute;background-repeat: no-repeat;background-size: cover; top: 100%;left: 0;";
- 
-}
-  else{
-    document.getElementById('container').style.cssText = 
-    "";
-  
-}
 
 
   
@@ -26,7 +16,17 @@ var goFS = document.getElementById('goFS');
     function () {
         if(fs === false){
             document.getElementById('container').requestFullscreen();
-           
+            if (mediaQuery.matches) {
+                document.getElementById('container').style.cssText = 
+                "transform: rotate(-90deg);transform-origin: left top;width: 100vh;height: 100vw;overflow-x: hidden;position:absolute;background-repeat: no-repeat;background-size: cover; top: 100%;left: 0;";
+             
+            }
+              else{
+                document.getElementById('container').style.cssText = 
+                "";
+              
+            }
+            
             fs = true;
         }
         if(fs === true){
