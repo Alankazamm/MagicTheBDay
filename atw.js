43605c9043798bee07805dd259a06e799c627b66
Alankazamm <bruno.alanc61@gmail.com>
2022-05-08 19:06:28 -0300

    reversing last 2 commit changes

--- atw.js
+++ atw.js
@@ -1,5 +1,5 @@
 let fs = false;
-const mediaQuery = window.matchMedia("(orientation: portrait)");
+
 
 
 
@@ -16,16 +16,8 @@ var goFS = document.getElementById('goFS');
     function () {
         if(fs === false){
             document.getElementById('container').requestFullscreen();
-            if (mediaQuery.matches) {
-                document.getElementById('container').style.cssText = 
-                "transform: rotate(-90deg);transform-origin: left top;width: 100vh;height: 100vw;overflow-x: hidden;position:absolute;background-repeat: no-repeat;background-size: cover; top: 100%;left: 0;";
-             
-            }
-              else{
-                document.getElementById('container').style.cssText = 
-                "";
-              
-            }
+           
+            
             
             fs = true;
         }
