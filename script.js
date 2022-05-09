c0ac9ea8858698aeb56472790f13bc033845de1c
Alankazamm <bruno.alanc61@gmail.com>
2022-05-08 21:52:59 -0300

    dsad

--- script.js
+++ script.js
@@ -5,16 +5,12 @@
 
  screen.orientation.addEventListener("change", function() {
             if(screen.orientation.type == 'landscape-primary'){
-                document.getElementById('bg-div').style.backgroundColor="blue";
-               // console.log(window.orientation);
-               //document.getElementById('bg-div').style.cssText = "transform: rotate(-90deg);transform-origin: left top;width: 100vh;height: 100vw;overflow-x: hidden;position:absolute;background-repeat: no-repeat;background-size: cover;top: 100%;left: 0;";
-              // document.getElementById('bg-div').style.backgroundImage = "url('./imgs/img-"+idSet+".jpg')";
+    
+               document.getElementById('bg-div').style.cssText = "transform: rotate(-90deg);transform-origin: left top;width: 100vh;height: 100vw;overflow-x: hidden;position:absolute;background-repeat: no-repeat;background-size: cover;top: 100%;left: 0;";
+               document.getElementById('bg-div').style.backgroundImage = "url('./imgs/img-"+idSet+".jpg')";
             }
              else if(screen.orientation.type  == 'portrait-primary'){
-                console.log(screen.orientation)
-                   document.getElementById('bg-div').style.backgroundColor="red";
-                            
-               
+           
                 document.getElementById('bg-div').style.backgroundImage = "url('./imgs/img-"+idSet+".jpg')";
              }
              else {
